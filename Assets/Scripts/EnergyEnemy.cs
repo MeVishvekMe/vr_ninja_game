e8b128b80d3529f0a822c58a8c6f7f6e12d24c73
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-10-11 15:37:19 +0530

    added gun sound

--- Assets/Scripts/EnergyEnemy.cs
+++ Assets/Scripts/EnergyEnemy.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,19 +9,9 @@ public class EnergyEnemy : MonoBehaviour {
     public Transform playerTransform;
     public Slider slider;
 
-    //// Enemy Movement
-    //public float speed = 2f; // Speed of movement
-    //private Vector3 targetPosition;
-
-    //public float minDistance = 20f;
-    //public float maxDistance = 50f;
-
     void Start() {
         _time = -20f;
         playerTransform = Camera.main.transform;
-
-        //targetPosition = GetRandomTarget();
-        //StartCoroutine(MoveToTarget());
     }
 
     void Update() {
@@ -36,37 +24,6 @@ public class EnergyEnemy : MonoBehaviour {
             _time = 0;
         }
     }
-
-    //private Vector3 GetRandomTarget() {
-    //    float theta = 2f * Mathf.PI * Random.value; // Azimuthal angle
-    //    float phi = Mathf.Acos(Random.value);       // Polar angle
-
-    //    // Generate random radius between minDistance and maxDistance
-    //    float r = minDistance + (maxDistance - minDistance) * Random.value;
-
-    //    // Convert to Cartesian coordinates
-    //    float x = r * Mathf.Sin(phi) * Mathf.Cos(theta);
-    //    float y = r * Mathf.Cos(phi);
-    //    float z = r * Mathf.Sin(phi) * Mathf.Sin(theta);
-
-    //    return new Vector3(x, y, z);
-    //}
-
-    //private IEnumerator MoveToTarget() {
-    //    while (true) {
-    //        // Move towards the target position
-    //        while (Vector3.Distance(transform.position, targetPosition) > 0.1f) {
-    //            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-    //            yield return null;
-    //        }
-
-    //        // Wait a moment at the target position
-    //        yield return new WaitForSeconds(0.1f);
-
-    //        // Get a new random target
-    //        targetPosition = GetRandomTarget();
-    //    }
-    //}
 
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Bullet") {
